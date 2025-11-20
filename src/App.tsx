fd6869906cf42848e5fe80cc4af19bb178e3668c
arturfrut <arturfruttero@gmail.com>
2025-11-20 17:58:15 -0300

    Add basename to BrowserRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/pixel-purple-solutions">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
